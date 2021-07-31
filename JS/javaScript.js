1d9b2bcaf8d1fb37cc71a30678392db6b7f3c89f
Abdel-rhman1 <yousef777906@gmail.com>
2021-08-01 00:45:50 +0200

    commiting full poject

---
+++ JS/javaScript.js
@@ -0,0 +1,8 @@
+function showNavBar(){
+    var x = document.getElementById("navbarItems");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
